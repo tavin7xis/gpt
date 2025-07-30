4fa8b77594244dee515a8904a7dc45afc9b65b44
whdddd <148370313+tavin7xis@users.noreply.github.com>
2025-07-30 10:54:09 -0300

    iufburfuh

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["Fernandão", "Jojo", "Negão da zs", "Caica", "Vinão", "Xaumer", "Gaybriel"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
